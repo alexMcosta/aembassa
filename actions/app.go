3e4adb67aa8a88c8eda8b46ee24ed9cbdc879e25
acosta <alexmcosta.dev@gmail.com>
2019-06-23 16:35:00 -0700

    Add New Relic instrumentation

--- actions/app.go
+++ actions/app.go
@@ -1,10 +1,13 @@
 package actions
 
 import (
+	"os"
+
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/envy"
 	forcessl "github.com/gobuffalo/mw-forcessl"
 	paramlogger "github.com/gobuffalo/mw-paramlogger"
+	newrelic "github.com/newrelic/go-agent"
 	"github.com/unrolled/secure"
 
 	"github.com/alexmcosta/aembassador/aembassa/models"
@@ -38,6 +41,28 @@ func App() *buffalo.App {
 		app = buffalo.New(buffalo.Options{
 			Env:         ENV,
 			SessionName: "_aembassa_session",
+		})
+
+		// Add New Relic
+		config := newrelic.NewConfig("AEMBASSA", os.Getenv("NEW_RELIC_LICENSE_KEY"))
+		config.Enabled = ENV == "development"
+		na, _ := newrelic.NewApplication(config)
+
+		app.Use(func(next buffalo.Handler) buffalo.Handler {
+			return func(c buffalo.Context) error {
+				req := c.Request()
+				txn := na.StartTransaction(req.URL.String(), c.Response(), req)
+				ri := c.Value("current_route").(buffalo.RouteInfo)
+				txn.AddAttribute("PathName", ri.PathName)
+				txn.AddAttribute("RequestID", c.Value("request_id"))
+				defer txn.End()
+				err := next(c)
+				if err != nil {
+					txn.NoticeError(err)
+					return err
+				}
+				return nil
+			}
 		})
 
 		// Automatically redirect to SSL
